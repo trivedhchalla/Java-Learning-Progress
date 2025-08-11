13eb2063553712852ce2f0bec489eb22116e8bfb
trivedhchalla <ctrivedh@gmail.com>
2025-08-11 13:57:45 +0530

    Day-1 of JAVA learning
    Topics studied:
    JAVA Architecture, JVM, JDK, JRE, class, method, variables, datatypes,

---
+++ Day_1.java
@@ -0,0 +1,15 @@
+class Day_1{
+    public static void main(String[] args){
+        // decoding the first program and main function & everything soon 
+        System.out.println("Hello World!!");
+        System.out.println(args[0]);
+        // variable declaration
+        int money = 90;
+        System.out.println(money);
+        money ++;
+        System.out.println(money);
+        // indentifiers rules
+        int $value = 87;
+        System.out.println($value);
+    }
+}
